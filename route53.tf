689e0c46928189ed09bbd172f916932373fa082a
datianshi <dsz0111@gmail.com>
2017-05-09 06:28:46 -0700

    init azure stuf

--- route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "cfrouter" {
-  zone_id = "${var.route53_zone_id}"
-  name = "*.cf.shaozhenpcf.com"
-  type = "CNAME"
-  ttl = "900"
-  records = ["${aws_elb.cfrouter.dns_name}"]
-}
